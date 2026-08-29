44293a2b8b7295115372da2aab3cd3a4f66a699f
Pbail Squad <156425176+Pbail@users.noreply.github.com>
2026-08-29 23:34:26 +0530

    Delete wa-telegram-bot 2 directory

--- wa-telegram-bot 2/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-async function connectDB() {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log('[DB] MongoDB connected');
-  return mongoose;
-}
-
-module.exports = { connectDB, mongoose };
